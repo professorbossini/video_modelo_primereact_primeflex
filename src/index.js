54c83699fecf0c7b6388875feda3bf100437dbd3
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-10-31 02:48:10 -0300

    Modelo inicial PrimeReact/PrimeFlex

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+//Escolha o tema que desejar
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
+import 'primeflex/primeflex.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      Modelo Inicial PrimeFlex/PrimeReact
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <App />,
+  document.querySelector('#root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
